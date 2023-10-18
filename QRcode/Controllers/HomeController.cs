8fc988ab8afa0942c2775f3dcae353b2fe5b0391
dave2ade <david.aderibigbe@africaprudential.com>
2023-10-18 22:45:23 +0100

    Frist Commit

---
+++ QRcode/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using IronBarCode;
+using Microsoft.AspNetCore.Mvc;
+using QRcode.Models;
+using System.Diagnostics;
+
+namespace QRcode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _iwebhostenvironment;
+
+        public HomeController(IWebHostEnvironment _webHostEnvironment)
+        {
+            _iwebhostenvironment = _webHostEnvironment;
+        }
+
+        public  IActionResult CreateQRCode()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateQRCode(GenerateQRCodeModel GenerateQRCode)
+        {
+            try
+            {
+                GeneratedBarcode barcode = QRCodeWriter.CreateQrCode(GenerateQRCode.QRInput, 200);
+                barcode.AddBarcodeValueTextBelowBarcode();
+                barcode.SetMargins(10);
+                barcode.ChangeBarCodeColor(Color.Blue);
+                string path = Path.Combine(_iwebhostenvironment.WebRootPath, "GeneratedQRCode");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                string filePath = Path.Combine(_iwebhostenvironment.WebRootPath, "GeneratedQRCode/qrcode.png");
+                barcode.SaveAsPng(filePath);
+                string filename = Path.GetFileName(filePath);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ QRcode/Models/GenerateQRCodeModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QRcode.Models
+{
+    public class GenerateQRCodeModel
+    {
+        [Required]
+        [Display(Name = "Enter Text")]
+        public string QRInput { get; set; }
+    }
+}
